ed21e2f8e04c53d6c4eec899e2cabd0822907f27
Silvena Voynova <sv@insomniacdesign.com>
2026-05-31 17:11:35 +0300

    feat: add seo and security

--- src/pages/sitemap.xml.ts
+++ src/pages/sitemap.xml.ts
@@ -9,12 +9,14 @@ const seoPaths = SUPPORTED_LANGS.flatMap((lang) =>
 const urls = ["/", ...homePaths, ...seoPaths];
 
 export function GET() {
+  const today = new Date().toISOString().split("T")[0];
   const body = `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
 ${urls
   .map(
     (path) => `  <url>
     <loc>${SITE_ORIGIN}${path === "/" ? "" : path}</loc>
+    <lastmod>${today}</lastmod>
   </url>`,
   )
   .join("\n")}
